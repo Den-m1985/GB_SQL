ad6772b4eddc9c44a67f59ac52273c0f3fb902f1
den-m1985 <denismartyanov244@gmail.com>
2023-04-17 15:37:12 +0200

    seminar

--- seminar1.sql
+++ seminar1.sql
@@ -5,17 +5,31 @@ comment
 */
 USE seminar1;
 
+
 SELECT * FROM student;
 
 SELECT name FROM student;
 
+
 SELECT * FROM student
 WHERE name = 'Hgj';
 
+
 SELECT name, phone_number FROM student;
 
--- % любая подстрокаalter   Galaxy S5, Galaxy S8s, Galaxy S10, Galaxy A10
+
+-- % любая подстрока (сколько угодно символов либо ничего)   Galaxy S5, Galaxy S8s, Galaxy S10, Galaxy A10
 -- LIKE 'Galaxy S%'  -> Galaxy S5, Galaxy S8, Galaxy S10
 --  _  одиноячный смволalter
 -- LIKE 'Galaxy S_'  -> Galaxy S5
+SELECT * FROM student
+WHERE name LIKE 'H%';
 
+SELECT * FROM student
+WHERE name LIKE '%g';
+
+SELECT * FROM student
+WHERE name LIKE '%H%';
+
+SELECT * FROM student
+WHERE name LIKE 'H%' OR name LIKE '_I_';
