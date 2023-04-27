f411502ed3eb762c89942e9c5d6d9a44830eae54
den-m1985 <denismartyanov244@gmail.com>
2023-04-27 17:02:05 +0200

    home_work_5

--- home_work_5.sql
+++ home_work_5.sql
@@ -24,19 +24,28 @@ SELECT * FROM Cars;
 1
 Создайте представление, в которое попадут автомобили стоимостью  до 25 000 долларов
 */
-
+DROP VIEW IF EXISTS CopyCars;
+CREATE VIEW CopyCars AS
+SELECT * FROM Cars WHERE Cost < 25000;
+SELECT * FROM CopyCars;
 
 /*
 2
 Изменить в существующем представлении порог для стоимости: 
 пусть цена будет до 30 000 долларов (используя оператор ALTER VIEW) 
 */
+ALTER VIEW CopyCars AS
+SELECT * FROM Cars WHERE Cost < 30000;
+SELECT * FROM CopyCars;
 
 /*
 3
 Создайте представление, в котором будут только автомобили марки “Шкода” и “Ауди”
 */
-
+DROP VIEW IF EXISTS Only_Skoda_Audi;
+CREATE VIEW Only_Skoda_Audi AS
+SELECT * FROM Cars WHERE Name IN ('Skoda', 'Audi');
+SELECT * FROM Only_Skoda_Audi;
 
 
 
@@ -47,52 +56,52 @@ SELECT * FROM Cars;
 DROP TABLE IF EXISTS Analysis;
 CREATE TABLE  Analysis
 (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	an_id INT NOT NULL, -- ID анализа
+	an_id INT AUTO_INCREMENT PRIMARY KEY, -- ID анализа
     an_name VARCHAR(20), -- название анализа
     an_cost INT NOT NULL, -- себестоимость анализа
     an_price INT NOT NULL, -- розничная цена анализа
-    an_group VARCHAR(20) -- группа анализов
+    an_group INT NOT NULL -- группа анализов
 );
-INSERT INTO Analysis (an_id, an_name, an_cost, an_price, an_group)
+INSERT INTO Analysis (an_name, an_cost, an_price, an_group)
 VALUES
-(10, 'Баралиоз', 1000, 2000, 'V'),
-(20, 'Маралиоз', 500, 3000, 'VI'),
-(30, 'Фаралиоз', 100, 4000, 'IV');
+('Баралиоз', 1000, 2000, 5),
+('Маралиоз', 500, 3000, 6),
+('Фаралиоз', 100, 4000, 4);
 SELECT * FROM Analysis;
 -- Есть таблица групп анализов Groups:
 DROP TABLE IF EXISTS Groups_s;
 CREATE TABLE  Groups_s
 (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	gr_id INT NOT NULL, --  ID группы
+	gr_id INT AUTO_INCREMENT PRIMARY KEY, --  ID группы
     gr_name VARCHAR(20), -- название группы
     gr_temp INT NOT NULL -- температурный режим хранения
 );
-INSERT INTO Groups_s (gr_id, gr_name, gr_temp)
+INSERT INTO Groups_s (gr_name, gr_temp)
 VALUES
-(5, 'Первоцвет', 50),
-(15, 'Нервоцвет', 10),
-(25, 'Гервоцвет', 2);
+('Первоцвет', 50),
+('Нервоцвет', 10),
+('Гервоцвет', 2);
 SELECT * FROM Groups_s;
 -- Есть таблица заказов Orders:
 DROP TABLE IF EXISTS Orders;
 CREATE TABLE  Orders
 (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	ord_id INT NOT NULL, --  ID заказа
+	ord_id INT AUTO_INCREMENT PRIMARY KEY, --  ID заказа
     ord_datetime DATE NOT NULL, -- дата и время заказа
     ord_an INT NOT NULL -- ID анализа
 );
-INSERT INTO Orders (ord_id, ord_datetime, ord_an)
+INSERT INTO Orders (ord_datetime, ord_an)
 VALUES
-(1, '1990-03-10', 50),
-(2, '1990-05-10', 40),
-(3, '1990-12-10', 30);
+('2020-02-4', 1),
+('2020-02-10', 2),
+('2021-12-10', 3);
 SELECT * FROM Orders;
 /*
 Вывести название и цену для всех анализов, которые продавались 5 февраля 2020 и всю следующую неделю.
 */
+SELECT * FROM Analysis
+JOIN Orders ON Analysis.an_id = Orders.ord_an 
+WHERE Orders.ord_datetime BETWEEN '2020-02-05' AND '2020-02-12';
 
 
 
@@ -104,18 +113,17 @@ CREATE TABLE  Train_timetable
 	Id INT AUTO_INCREMENT PRIMARY KEY,
     train_id INT NOT NULL,
 	staition VARCHAR(20), 
-	staition_time TIME(0),
-    time_to_next_station TIME(0)
+	staition_time TIME(0)
 );
-INSERT INTO Train_timetable (train_id, staition, staition_time, time_to_next_station)
+INSERT INTO Train_timetable (train_id, staition, staition_time)
 VALUES
-(110, 'San Francisco', '10:00:00', '00:54:00'),
-(110, 'Redwood City', '10:54:00', '00:08:00'),
-(110, 'Palo Alto', '11:02:00', '01:33:00'),
-(110, 'San Jose', '12:35:00', ''),
-(120, 'San Francisco', '11:00:00', '01:49:00'),
-(120, 'Palo Alto', '12:49:00', '01:49:00'),
-(120, 'San Jose', '13:30:00', '');
+(110, 'San Francisco', '10:00:00'),
+(110, 'Redwood City', '10:54:00'),
+(110, 'Palo Alto', '11:02:00'),
+(110, 'San Jose', '12:35:00'),
+(120, 'San Francisco', '11:00:00'),
+(120, 'Palo Alto', '12:49:00'),
+(120, 'San Jose', '13:30:00');
 SELECT * FROM Train_timetable;
 /*
 Добавьте новый столбец под названием «время до следующей станции». 
@@ -127,3 +135,8 @@ SELECT * FROM Train_timetable;
 результат. В этом случае функция сравнивает значения в столбце 
 «время» для станции со станцией сразу после нее.
 */
+SELECT *,
+LEAD(staition_time, 1, NULL) OVER (PARTITION BY train_id ORDER BY staition_time)
+AS time_to_next_staition
+FROM Train_timetable;
+
